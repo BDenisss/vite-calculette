be7b2dee6e0a4bc2f5ea431cfc257fe3403b491b
Denis Bucspun <bucspun.d@gmail.com>
2024-11-05 10:29:54 +0100

    fix: remove src folder and resolve directory structure issue

--- main.js
+++ main.js
@@ -1,25 +1,41 @@
-document.getElementById("calculate").addEventListener("click", () => {
-  const num1 = parseFloat(document.getElementById("num1").value);
-  const num2 = parseFloat(document.getElementById("num2").value);
-  const operation = document.getElementById("operation").value;
-  let result;
+import './style.css'
 
-  switch (operation) {
-    case "+":
-      result = num1 + num2;
-      break;
-    case "-":
-      result = num1 - num2;
-      break;
-    case "*":
-      result = num1 * num2;
-      break;
-    case "/":
-      result = num2 !== 0 ? num1 / num2 : "Erreur: division par zéro";
-      break;
-    default:
-      result = "Opération inconnue";
+console.log("main.js chargé");
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM prêt");
+  const calculateButton = document.getElementById("calculate");
+  console.log("Bouton calcul : ", calculateButton);
+  
+  if (calculateButton) {
+    calculateButton.addEventListener("click", () => {
+      const num1 = parseFloat(document.getElementById("num1").value);
+      const num2 = parseFloat(document.getElementById("num2").value);
+      const operation = document.getElementById("operation").value;
+      let result;
+
+      switch (operation) {
+        case "+":
+          result = num1 + num2;
+          break;
+        case "-":
+          result = num1 - num2;
+          break;
+        case "*":
+          result = num1 * num2;
+          break;
+        case "/":
+          result = num2 !== 0 ? num1 / num2 : "Erreur: division par zéro";
+          break;
+        default:
+          result = "Opération inconnue";
+      }
+
+      document.getElementById("result").innerText = `Résultat : ${result}`;
+    });
+  } else {
+    console.error("Le bouton de calcul n'a pas été trouvé dans le DOM.");
   }
-
-  document.getElementById("result").innerText = `Résultat : ${result}`;
 });
+
+
